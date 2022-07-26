fb4a6a8b22da4e005a7c85af6aee08135637bfba
ralexrivero <ralexrivero@gmail.com>
2022-07-26 19:13:12 -0300

    loader

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter-button');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading animation and hide container
+function loading () {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading and show container
+function complete () {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // Show new quote
 function newQuote () {
+  loading();
   // pick a random quote from apiQuote array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (quote.text.length > 70) {
@@ -21,10 +35,12 @@ function newQuote () {
   } else {
     authorText.textContent = quote.author;
   }
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes () {
+  loading();
   const apiURL = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiURL);
