0de5b2f481c9de42bfca8575a65e2c446d9e7277
ralexrivero <ralexrivero@gmail.com>
2022-07-27 01:58:41 -0300

    loader

--- quote-generator2/scripts.js
+++ quote-generator2/scripts.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterButton = document.getElementById('twitter-button');
 const quoteBtn = document.getElementById('quote-btn');
+const loader = document.getElementById('loader');
+
+// loading
+function loading () {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete () {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
 // get quote from API
 
 async function getQuote () {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // proxy for cross-origin requests
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -21,12 +37,22 @@ async function getQuote () {
     if (data.quoteAuthor === '') {
       quoteAuthor.innerText = 'Unknown';
     } else {
-    quoteAuthor.innerText = data.quoteAuthor;
+      quoteAuthor.innerText = data.quoteAuthor;
     }
+    complete();
   } catch (error) {
     getQuote();
   }
 }
+
+function tweetQuote () {
+  const twitterUrl = `https://twitter.com/intent/tweet?text="${quoteText.innerText}"%0A${quoteAuthor.innerHTML}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// event listener
+quoteBtn.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
 // on load
 getQuote();
