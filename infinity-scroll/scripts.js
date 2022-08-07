3d681c8bb401b198a1a2991f07c0262a829d20d9
ralexrivero <ralexrivero@gmail.com>
2022-08-07 03:09:53 -0300

    implemet unsplash api

---
+++ infinity-scroll/scripts.js
@@ -0,0 +1,52 @@
+const imageContainer = document.getElementById('image-container');
+/* const loader = document.getElementById('loader'); */
+
+let photosArray = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = 'Eq7N_bjki_HpNR8C_AOGxTKgvvjNGlEjPtPfuc4LCFM';
+const orient = 'landscape';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orient}`;
+
+// helper function to set attributes on DOM elements
+function setAttr (element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create elements for links and photos, add to DOM
+function displayPhotos () {
+  photosArray.forEach((photo) => {
+    const anchor = document.createElement('a');
+    setAttr(anchor, {
+      href: photo.links.html,
+      target: '_blank'
+    });
+    const img = document.createElement('img');
+    setAttr(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.description
+    });
+    anchor.appendChild(img);
+    imageContainer.appendChild(anchor);
+  });
+}
+
+// Get photos from Unsplash API
+
+async function getPhotos () {
+  try {
+    const response = await fetch(apiURL);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load
+
+getPhotos();
